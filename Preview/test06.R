e8a782d40277fe02243619633a2d775a6b790bb3
Walla-B <81977508+Walla-B@users.noreply.github.com>
2021-08-30 23:58:59 +0900

    Preview06

---
+++ Preview/test06.R
@@ -0,0 +1,26 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: dev
+# Created on: 2021-08-30
+
+# for installed & installing packages
+
+# install.packages("ggplot2")
+# install.packages("sp")
+# # rgdal 패키지
+# install.packages("rgdal")
+# # rgeos 패키지
+# install.packages("rgeos")
+# # ggmap 패키지
+# install.packages("ggmap")
+# # tmap 패키지
+# install.packages("tmap")
+# # spatstat 패키지
+# install.packages("spatstat")
+# # raster 패키지
+# install.packages("raster")
+# # spdep 패키지
+# install.packages("spdep")
+# # gstat 패키지
+# install.packages("gstat")
+# install.packages("spgwr")
---
+++ Preview/test07.R
@@ -0,0 +1,14 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: dev
+# Created on: 2021-08-30
+
+library(datasets)
+
+head(iris)
+
+plot(iris$Species)
+plot(iris$Petal.Length)
+plot(iris$Species, iris$Petal.Width)
+plot(iris$Petal.Length, iris$Petal.Width)
+plot(iris)
