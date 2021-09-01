004338c80f1c59da80925dd3b69ce0864ee88897
Walla-B <81977508+Walla-B@users.noreply.github.com>
2021-09-01 23:17:51 +0900

    Preview07

--- Preview/test07.R
+++ Preview/test07.R
@@ -12,3 +12,24 @@ plot(iris$Petal.Length)
 plot(iris$Species, iris$Petal.Width)
 plot(iris$Petal.Length, iris$Petal.Width)
 plot(iris)
+
+# plot with options
+plot(iris$Petal.Length, iris$Petal.Width,
+     col = "#cc0000",
+     pch = 19,
+     main = "Iris: Petal Length vs Petal Width",
+     xlab = "Petal Length",
+     ylab = "Petal Width" )
+
+# formulas in plot()
+plot(cos, 0, 2*pi)
+plot(exp, 1, 5)
+plot(dnorm, -3, +3)
+
+plot(dnorm, -3, +3,
+     col = "#cc0000",
+     lwd = 5,
+     main = "Std norm distribution",
+     xlab = "xlabel",
+     ylab = "ylabel")
+
